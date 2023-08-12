34f4c63568ca20ec0856af22839425f8a64aefc8
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-12 09:17:01 -0700

    Create city.py

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+    This func defines the classes of City.
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Representingss the City."""
+
+    state_id = ""
+    name = ""
