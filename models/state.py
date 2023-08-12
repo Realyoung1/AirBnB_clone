b607e4415191dbc03a0390d3c09f08a432852264
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-12 09:21:15 -0700

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+    This func defines the classes of State.
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Representingss the State."""
+
+    name = ""
