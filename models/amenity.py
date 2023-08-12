b25dbe6375b9cde132d525ce33e2e7ac9573e3a3
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-12 09:11:57 -0700

    Create amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+    This func defines the classes of Amenitys.
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Represent a Amenity."""
+
+    name = ""
